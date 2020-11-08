1fdf1c17271e070acab1d18781cd203d8361eb77
Ravi Simon Shah <infinite.ravi@gmail.com>
2020-11-08 18:39:21 +0100

    NG-1169 Implement join twilio room method in plugin

--- ios/Plugin/Plugin.m
+++ ios/Plugin/Plugin.m
@@ -3,4 +3,6 @@
 
 // Define the plugin using the CAP_PLUGIN Macro, and
 // each method the plugin supports using the CAP_PLUGIN_METHOD macro.
-CAP_PLUGIN(TwilioVideoPlugin, "TwilioVideoPlugin")
+CAP_PLUGIN(TwilioVideoPlugin, "TwilioVideoPlugin",
+  CAP_PLUGIN_METHOD(joinTwilioRoom, CAPPluginReturnPromise);
+)
--- ios/Plugin/Plugin.swift
+++ ios/Plugin/Plugin.swift
@@ -6,4 +6,40 @@ import Capacitor
  * here: https://capacitorjs.com/docs/plugins/ios
  */
 @objc(TwilioVideoPlugin)
-public class TwilioVideoPlugin: CAPPlugin {}
+public class TwilioVideoPlugin: CAPPlugin {
+
+    var viewController: TwilioVideoViewController?
+
+    @objc func joinTwilioRoom(_ call: CAPPluginCall) {
+
+        guard let roomName = call.options["roomName"] as? String else {
+            call.reject("roomName is required")
+            return
+        }
+
+        guard let accessToken = call.options["accessToken"] as? String else {
+            call.reject("accessToken is required")
+            return
+        }
+
+        let podBundle = Bundle(for: TwilioVideoViewController.self)
+        let bundleURL = podBundle.url(forResource: "Plugin", withExtension: "bundle")!
+        let bundle = Bundle(url: bundleURL)!
+
+        if self.viewController != nil {
+            NSLog("Unexpected behaviour: a view already exists")
+        }
+
+        self.viewController = TwilioVideoViewController(nibName: "TwilioVideoViewController", bundle: bundle)
+        self.viewController!.roomName = roomName
+        self.viewController!.accessToken = accessToken
+        self.viewController!.modalPresentationStyle = UIModalPresentationStyle.fullScreen
+
+        DispatchQueue.main.sync {
+            self.bridge.viewController.present(self.viewController!, animated: false, completion: nil)
+        }
+
+        call.resolve()
+    }
+
+}
